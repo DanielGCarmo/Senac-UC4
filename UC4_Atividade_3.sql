aeb51068dc876555e89791cb2868a470726d21d1
Daniel Gibson Rocha Candeira do Carmo <danielgrccarmo@gmail.com>
2023-06-15 11:36:15 -0300

    Update UC4_Atividade_3.sql

--- UC4_Atividade_3.sql
+++ UC4_Atividade_3.sql
@@ -6,19 +6,16 @@
 Percebe-se que o banco de dados para o sistema de lojas enfrenta alguns “gargalos” de desempenho, de modo que as operações no sistema estão demorando mais do que o esperado – algumas delas acontecem com frequência. Você tem a tarefa de propor melhorias a algumas dessas consultas, tornando-as mais eficientes.*/
  
 /* Atividade
-Utilizando o banco de dados proposto para as atividades, realize as consultas expostas no arquivo script_consultas.sql disponível em Conteúdos > Material complementar.
+Utilizando o banco de dados proposto para as atividades, realize as consultas expostas no arquivo UC4_Atividade_3_Script_consultas.
 
 Para cada consulta presente no script, faça o seguinte:
  
-	1. Execute o comando EXPLAIN mostrando detalhes da execução da consulta e tire um print.
+	1. Execute o comando EXPLAIN mostrando detalhes da execução da consulta.
 	2. Realize ajustes de otimização nas consultas utilizando JOINs entre as tabelas assim como removendo os asteriscos ( * ) e definindo quais colunas devem ser retornadas de cada consulta afim de otimiza-la.
-	3. Crie índices que possam se beneficiar do recurso para melhorar o desempenho das consultas, considerando principalmente colunas utilizadas nas cláusulas Where.
-	4. Execute novamente o comando EXPLAIN mostrando agora os detalhes da execução da consulta, depois das otimizações realizadas e tire um print.
-	5. Crie uma view para cada uma das consultas ajustadas com JOINS.
+	3. Crie índices que possam se beneficiar do recurso para melhorar o desempenho das consultas, considerando principalmente colunas utilizadas nas cláusulas WHERE.
+	4. Execute novamente o comando EXPLAIN mostrando agora os detalhes da execução da consulta, depois das otimizações realizadas.
+	5. Crie uma VIEW para cada uma das consultas ajustadas com JOINS.
 	6. Armazene as consultas ajustadas e os demais comandos em scripts .sql para entrega. Utilize comentários para organizar e explicar seus scripts.*/
- 
-/* Entrega
-Envie, no local destinado à entrega, até a data indicada no cronograma de estudos, os scripts .sql desenvolvidos na atividade.*/
  
 /* Dica de leitura
 Para esta atividade, leia os seguintes materiais:
@@ -48,10 +45,9 @@ SELECT *
 FROM venda v, item_venda iv, produto p, cliente c, funcionario f
 WHERE v.id = iv.venda_id AND c.id = v.cliente_id AND p.id = iv.produto_id AND f.id = v.funcionario_id AND tipo_pagamento = 'D';
 -- Explicação:
-	-- * A consulta utiliza as tabelas "venda", "item_venda", "produto", "cliente" e "funcionario".
-	-- * A condição WHERE filtra as vendas pagas em dinheiro.
-	-- * O comando EXPLAIN irá fornecer informações sobre a execução da consulta, como a ordem de leitura das tabelas, o uso de índices e outras estatísticas relevantes.
--- Print: ´uc4atividade3-1_Print1´
+	-- A consulta utiliza as tabelas "venda", "item_venda", "produto", "cliente" e "funcionario".
+	-- A condição WHERE filtra as vendas pagas em dinheiro.
+	-- O comando EXPLAIN irá fornecer informações sobre a execução da consulta, como a ordem de leitura das tabelas, o uso de índices e outras estatísticas relevantes.
 
 -- Consulta para encontrar todas as vendas de produtos de um dado fabricante:
 EXPLAIN
@@ -60,11 +56,10 @@ FROM produto p, item_venda iv, venda v
 WHERE p.id = iv.produto_id AND v.id = iv.venda_id AND p.fabricante LIKE '%lar%'
 ORDER BY p.nome;
 -- Explicação:
-	-- * A consulta utiliza as tabelas "produto", "item_venda" e "venda".
-	-- * A condição WHERE filtra os produtos de um dado fabricante (utilizando o operador LIKE com um curinga '%').
-	-- * A ordenação é feita pelo nome do produto.
-	-- * O comando EXPLAIN irá fornecer informações sobre a execução da consulta, como a ordem de leitura das tabelas, o uso de índices e outras estatísticas relevantes.
--- Print: ´uc4atividade3-1_Print2´
+	-- A consulta utiliza as tabelas "produto", "item_venda" e "venda".
+	-- A condição WHERE filtra os produtos de um dado fabricante (utilizando o operador LIKE com um curinga '%').
+	-- A ordenação é feita pelo nome do produto.
+	-- O comando EXPLAIN irá fornecer informações sobre a execução da consulta, como a ordem de leitura das tabelas, o uso de índices e outras estatísticas relevantes.
 
 -- Relatório de vendas de produto por cliente:
 EXPLAIN
@@ -73,12 +68,11 @@ FROM produto p, item_venda iv, venda v, cliente c
 WHERE p.id = iv.produto_id AND v.id = iv.venda_id AND c.id = v.cliente_id
 GROUP BY c.nome, p.nome;
 -- Explicação:
-	-- * A consulta utiliza as tabelas "produto", "item_venda", "venda" e "cliente".
-	-- * A condição WHERE estabelece as relações entre as tabelas.
-	-- * A função de agregação SUM é utilizada para calcular o valor total e a quantidade total de vendas por produto e cliente.
-	-- * A cláusula GROUP BY agrupa os resultados por nome do cliente e nome do produto.
-	-- * O comando EXPLAIN irá fornecer informações sobre a execução da consulta, como a ordem de leitura das tabelas, o uso de índices e outras estatísticas relevantes.
--- Print: ´uc4atividade3-1_Print3´
+	-- A consulta utiliza as tabelas "produto", "item_venda", "venda" e "cliente".
+	-- A condição WHERE estabelece as relações entre as tabelas.
+	-- A função de agregação SUM é utilizada para calcular o valor total e a quantidade total de vendas por produto e cliente.
+	-- A cláusula GROUP BY agrupa os resultados por nome do cliente e nome do produto.
+	-- O comando EXPLAIN irá fornecer informações sobre a execução da consulta, como a ordem de leitura das tabelas, o uso de índices e outras estatísticas relevantes.
 
 -- -----------------------------------------------------
 -- 2. Realize ajustes de otimização nas consultas utilizando JOINs entre as tabelas assim como removendo os asteriscos ( * ) e definindo quais colunas devem ser retornadas de cada consulta afim de otimiza-la.
@@ -95,11 +89,11 @@ JOIN funcionario f ON f.id = v.funcionario_id
 WHERE v.tipo_pagamento = 'D'
 ORDER BY v.data DESC;
 -- Explicação:
-	-- * Utilizou-se o operador JOIN para unir as tabelas relacionadas.
-    -- * Especificou-se as colunas a serem retornadas, evitando o uso do asterisco (*).
-    -- * Utilizou-se aliases para renomear as colunas, tornando o resultado mais legível.
-    -- * A condição WHERE filtra as vendas pagas em dinheiro.
-    -- * A ordenação é feita pela data de venda, das mais recentes para as mais antigas.
+	-- Utilizou-se o operador JOIN para unir as tabelas relacionadas.
+	-- Especificou-se as colunas a serem retornadas, evitando o uso do asterisco (*).
+	-- Utilizou-se aliases para renomear as colunas, tornando o resultado mais legível.
+	-- A condição WHERE filtra as vendas pagas em dinheiro.
+	-- A ordenação é feita pela data de venda, das mais recentes para as mais antigas.
 
 -- Consulta para encontrar todas as vendas de produtos de um dado fabricante:
 EXPLAIN
@@ -110,11 +104,11 @@ JOIN venda v ON v.id = iv.venda_id
 WHERE p.fabricante LIKE '%lar%'
 ORDER BY p.nome;
 -- Explicação:
-	-- * Utilizou-se o operador JOIN para unir as tabelas relacionadas.
-    -- * Especificou-se as colunas a serem retornadas, evitando o uso do asterisco (*).
-    -- * Utilizou-se aliases para renomear as colunas, tornando o resultado mais legível.
-    -- * A condição WHERE filtra os produtos de um dado fabricante.
-    -- * A ordenação é feita pelo nome do produto.
+	-- Utilizou-se o operador JOIN para unir as tabelas relacionadas.
+	-- Especificou-se as colunas a serem retornadas, evitando o uso do asterisco (*).
+	-- Utilizou-se aliases para renomear as colunas, tornando o resultado mais legível.
+	-- A condição WHERE filtra os produtos de um dado fabricante.
+	-- A ordenação é feita pelo nome do produto.
 
 -- Relatório de vendas de produto por cliente:
 EXPLAIN
@@ -125,11 +119,11 @@ JOIN venda v ON v.id = iv.venda_id
 JOIN cliente c ON c.id = v.cliente_id
 GROUP BY c.nome, p.nome;
 -- Explicação:
-	-- * Utilizou-se o operador JOIN para unir as tabelas relacionadas.
-    -- * Especificou-se as colunas a serem retornadas, evitando o uso do asterisco (*).
-    -- * Utilizou-se aliases para renomear as colunas, tornando o resultado mais legível.
-    -- * A função de agregação SUM é utilizada para calcular o valor total e a quantidade total de vendas por produto e cliente.
-    -- * A cláusula GROUP BY agrupa os resultados por nome do cliente e nome do produto.
+	-- Utilizou-se o operador JOIN para unir as tabelas relacionadas.
+	-- Especificou-se as colunas a serem retornadas, evitando o uso do asterisco (*).
+	-- Utilizou-se aliases para renomear as colunas, tornando o resultado mais legível.
+	-- A função de agregação SUM é utilizada para calcular o valor total e a quantidade total de vendas por produto e cliente.
+	-- A cláusula GROUP BY agrupa os resultados por nome do cliente e nome do produto.
 
 -- -----------------------------------------------------
 -- 3. Crie índices que possam se beneficiar do recurso para melhorar o desempenho das consultas, considerando principalmente colunas utilizadas nas cláusulas WHERE.
@@ -172,7 +166,6 @@ JOIN produto p ON p.id = iv.produto_id
 JOIN cliente c ON c.id = v.cliente_id
 WHERE v.tipo_pagamento = 'D'
 ORDER BY v.data DESC;
--- Print: ´uc4atividade3-4_Print1´
 
 -- Consulta para encontrar todas as vendas de produtos de um dado fabricante:
 EXPLAIN
@@ -182,7 +175,6 @@ JOIN item_venda iv ON p.id = iv.produto_id
 JOIN venda v ON v.id = iv.venda_id
 WHERE p.fabricante LIKE '%lar%'
 ORDER BY p.nome;
--- Print: ´uc4atividade3-4_Print2´
 
 -- Relatório de vendas de produto por cliente:
 EXPLAIN
@@ -192,7 +184,6 @@ JOIN venda v ON c.id = v.cliente_id
 JOIN item_venda iv ON v.id = iv.venda_id
 JOIN produto p ON p.id = iv.produto_id
 GROUP BY c.nome, p.nome;
--- Print: ´uc4atividade3-4_Print3´
 
 -- -----------------------------------------------------
 -- 5. Crie uma view para cada uma das consultas ajustadas com JOINS.
